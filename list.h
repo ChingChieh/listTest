d41bef41a1ec654ccab8c7e90a0d49fe7ab3112e
ChingChieh <garnett.huang@gmail.com>
2018-11-15 18:31:16 +0800

    Initial version

---
+++ list.h
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct rec {
+    char *value;
+    struct rec *next;
+} record;
+
+void insert_sorted(record **r, const char *value)
+{
+    record *newrec = NULL;
+    while (*r && strcmp(value, (*r)->value) > 0)
+        r = &((*r)->next);
+    newrec = malloc(sizeof(record));
+    newrec->value = strdup(value);
+    newrec->next = *r;
+    *r = newrec;                                                                                                                                             
+}
+
+void deleteNODE(record **node, const char *value){                                                                                
+    while((*node)->next){
+        if(!strcmp(value,(*node)->next->value)){
+            record *delete = (*node)->next;
+            (*node)->next = (*node)->next->next;
+            free(delete);
+            break;
+        }   
+        node = &((*node)->next);
+    }   
+}
+
+record* searchNODE(record *node, const char *value){
+    while(node){
+        if(!strcmp(value,node->value)){
+            return node;
+        }
+    }
+    return NULL;
+}
+
