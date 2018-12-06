5b197b6cf25b669169b18a8259e9e6606666eebe
ChingChieh <garnett.huang@gmail.com>
2018-12-06 14:59:16 +0800

    Second version

--- insertNODE.c
+++ insertNODE.c
@@ -3,39 +3,33 @@
 #include <stdio.h>
 
 int main() {
+	char *a_str = "atalanta";
+	char *b_str = "boston";
+	char *c_str = "chicago";
+	char *d_str = "detroit";
+	
+	char *in_str1 = "abc";
+	char *in_str2 = "abcd";
+	record *head = NULL;
 
-  record *head;
-  head = (record *)malloc(sizeof(record));
+	insert_sorted(&head, a_str);
+	insert_sorted(&head, b_str);
+	insert_sorted(&head, c_str);
+	insert_sorted(&head, d_str);
+	assert(head != NULL);
 
-  record *node1;
-  node1 = (record *)malloc(sizeof(record));
-  node1->value = "agsfgsfdg";
-  head->next = node1;
+	int i = 0;
+	record *iterator = head;
+	while(iterator){
+		assert((iterator->value)[0] == 'a' + i);
+		i++;
+		iterator = iterator->next;
+	}
+	
+	insert_sorted(&head, in_str1);
+	assert(!strcmp(head->value,in_str1));	
+	insert_sorted(&head, in_str2);
+	assert(!strcmp(head->next->value,in_str2));	
 
-  record *node2;
-  node2 = (record *)malloc(sizeof(record));
-  node2->value = "bggininder";
-  node1->next = node2;
-  node2->next = NULL;
-
-  insert_sorted(&(head->next), "cccchaha");
-
-  int i = 'a';
-  record *iterator = head->next;
-  while (iterator) {
-    assert(i == (iterator->value)[0]);
-    iterator = iterator->next;
-    i++;
-  }
-  iterator = head->next;
-  record *iter_next;
-  while (iterator) {
-    iter_next = iterator->next;
-    free(iterator);
-    iterator = iter_next;
-  }
-  head->next = NULL;
-  insert_sorted(&(head->next), "haha");
-  assert(!strcmp(head->next->value, "haha"));
-  return 0;
+	return 0;
 }
